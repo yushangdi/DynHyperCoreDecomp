64576bf792ed0080b066df341e3949e535e18356
sy <yushangdi123@Gmail.com>
2021-02-01 04:25:27 +0000

    change eps calc

--- FullyDynamic.cpp
+++ FullyDynamic.cpp
@@ -40,7 +40,7 @@ This program is developed on Windows. "-lpsapi" is related to reporting memory u
 #include "get_time.h"
 using namespace std;
 
-#define ROUND_NUM 4
+#define ROUND_NUM 1
 
 
 inline string precisionString(double a){
@@ -435,7 +435,8 @@ int main(int argc, char **argv) {
     std::cout << "output to " << outFileName << std::endl;
 	cout << "start running ..." << endl;
 
-	double epsilon_sun =1.0/2.0/lambda*(1+epsilon) + epsilon/3;// ((2+3/lambda)*(1+epsilon)/2-1)/3 ;//
+	double epsilon_sun = 1.0/2.0/(double)lambda;
+	// 1.0/2.0/lambda*(1+epsilon) + epsilon/3;// ((2+3/lambda)*(1+epsilon)/2-1)/3 ;//
 	// cout << epsilon_sun << endl; exit(1);
 	double alpha = 2 * (1 + 3 * epsilon_sun); /// theoretically correct alpha
 
